b0a481469f6e59458715dea0aa9fe8e95b13e75d
BKasuba <bendeguz.kasuba@gmail.com>
2025-06-08 23:22:40 +0100

    Enabled user sign-up using django's built in user creation

--- demo_app/forms.py
+++ demo_app/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from demo_app.models import Cars
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class CarsForm(forms.ModelForm):
     class Meta:
         model = Cars
         fields = "__all__"   # NOTE: the trailing comma is required
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2") #Django has built in password confirmation, password2 is not stored, only used for confirmation
--- demo_app/urls.py
+++ demo_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from demo_app import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("database/", views.database, name="database")
+    path("database/", views.database, name="database"),
+    path("login/", auth_views.LoginView.as_view(template_name="demo_app/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
+    path("signup/", views.signup, name="signup"),
 ]
