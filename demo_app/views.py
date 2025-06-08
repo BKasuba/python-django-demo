d266e4851184a366a6c25648c911d3fda71e0957
BKasuba <bendeguz.kasuba@gmail.com>
2025-06-08 14:08:04 +0100

    Complete basic Cars table display, update stylesheet for table, add hamter favicon

--- demo_app/views.py
+++ demo_app/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import redirect, render
 from demo_app.forms import CarsForm
+from demo_app.models import Cars
 
 def home(request):
     return render(request, "demo_app/home.html")
 
 def database(request):
-    form = CarsForm(request.POST or None)
-
+    form = CarsForm(request.POST or None) # this fetches the form from CarsForm
+    cars = Cars.objects.all()
     if request.method == "POST":
         if form.is_valid():
             message = form.save(commit=False)
             message.save()
             return redirect("database")
     else:
-        return render(request, "demo_app/databases.html", {"form":form})
+        return render(request, "demo_app/databases.html", {
+            "form":form,
+            "cars":cars}) # here the destination template is chosen and the context of the form and Cars model are passed in so data can be read 
