82787a65b82e851f7ce23080a410920a4c3dfc10
BKasuba <bendeguz.kasuba@gmail.com>
2025-06-08 23:22:40 +0100

    Enabled user sign-up using django's built in user creation

--- demo_app/views.py
+++ demo_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from demo_app.forms import CarsForm
 from demo_app.models import Cars
+from .forms import SignUpForm
 
 def home(request):
     return render(request, "demo_app/home.html")
@@ -17,3 +18,13 @@ def database(request):
         return render(request, "demo_app/databases.html", {
             "form":form,
             "cars":cars}) # here the destination template is chosen and the context of the form and Cars model are passed in so data can be read 
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect("login")
+    else:
+        form = SignUpForm()
+    return render(request, "demo_app/signup.html", {"form": form})
